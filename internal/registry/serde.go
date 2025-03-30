af5c67566ac855c79a82630a70fb9d16ddae7949
fbriansyah <fbriansyah@gmail.com>
2025-03-31 05:18:51 +0700

    add json and proto serdes

--- internal/registry/serde.go
+++ internal/registry/serde.go
@@ -3,5 +3,5 @@ package registry
 type Serde interface {
 	Register(v Registrable, options ...BuildOption) error
 	RegisterKey(key string, v any, options ...BuildOption) error
-	RegisterFactory(key string, fn func() any, options ...BuildOption)
+	RegisterFactory(key string, fn func() any, options ...BuildOption) error
 }
